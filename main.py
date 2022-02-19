3a3e87fa7e1f09c8296710e8ec3a310c7cdc96a7
goldbocman <hxfcndjd863@gmail.com>
2022-02-19 14:10:59 +0200

    task1

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
